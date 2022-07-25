9ece83f012dc79988b6a3773c27d4a8ab2500d91
mujak27 <mujakoverflow@gmail.com>
2022-07-25 10:30:43 +0700

    tried remove apollo graphql

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,33 @@
 // import { ApolloServer } from 'apollo-server';
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
-import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
-import { createContext } from './backend//utils/context';
-import { schema as nexusSchema } from './backend/types/nexusSchemaGen';
-import path from 'path'
+import path from 'path';
 
 const app = express();
 
 
 (async()=>{
+  // const apolloServer = new ApolloServer({
+  //   // schema,
+  //   schema: nexusSchema,
+  //   plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+  //   context: createContext
+  // })
   
-  const expressPort = process.env.PORT
-  console.log(expressPort);
-  app.listen(expressPort, ()=>{
-    console.log(`express started at ${expressPort}`);
-  })
-  
-  const apolloServer = new ApolloServer({
-    // schema,
-    schema: nexusSchema,
-    plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
-    context: createContext
-  })
-  
-  await apolloServer.start()
-  apolloServer.applyMiddleware({
-    app : app,
-  })
+  // await apolloServer.start()
+  // apolloServer.applyMiddleware({
+  //   app : app,
+  // })
+
   app.use(express.static(path.resolve(__dirname, '../build/')));
   
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
   });
+
+  const expressPort = process.env.PORT
+  console.log(expressPort);
+  app.listen(expressPort, ()=>{
+    console.log(`express started at ${expressPort}`);
+  })
 })()
 
